86e7e087583f94e077a36b71ca867c866b10db4d
Roujel Williams <sisilicon28@gmail.com>
2018-06-24 00:17:45 -0500

    Updated plugin code
    And created a buoyant body icon.

--- water_pack/addons/water_pack/main.gd
+++ water_pack/addons/water_pack/main.gd
@@ -1,37 +1,65 @@
 tool
 extends EditorPlugin
 
-var editor_camera;
-var water_physic = preload("./script/water_physic.gd")
+var editor_camera
+
+var water_physics = preload("./script/water_physics.gd")
 var buoyancy_gizmo = preload("./script/buoyancy_gizmo.gd")
 var viewport_size
-func forward_spatial_gui_input(p_camera, p_event):
-	editor_camera = p_camera
-	
 
-func mirror(origin, target, mirror_plane):
-	target.transform = mirror_plane.get_global_transform().affine_inverse()*origin.get_global_transform()
-	target.transform.origin.y *= -1
-	target.transform.basis.y.x *= -1
-	target.transform.basis.x.y *= -1
-	target.transform.basis.z.y *= -1
-	target.transform.basis.y.z *= -1
+var auto_buoy_button
+
+signal input(event)
+
+func forward_spatial_gui_input(p_camera, p_event):
+	if not editor_camera: editor_camera = p_camera
 
 func _ready():
 	viewport_size = get_viewport().size
 	
 func _enter_tree():
-	name = 'WaterPackPlugin'	
+	name = 'WaterPackPlugin'
+	
 	print("water plugin enter tree")
-	set_input_event_forwarding_always_enabled()
+	add_custom_type("BuoyantBody", "RigidBody", water_physics, preload("buoyant_body.png"))
 	add_custom_type("Water", "MeshInstance", preload("./script/water.gd"), preload("water.png"))
 	add_custom_type("BuoyancyPoints", "Resource", preload("./script/buoyancy_points.gd"), preload("water.png"))
+	
+	#auto_buoy_button = preload('./editor_gui_stuff/GenBuoyancyButton.tscn')
+	#add_control_to_container(CONTAINER_SPATIAL_EDITOR_MENU, auto_buoy_button)
+	#auto_buoy_button.hide()
+	
+	if not ProjectSettings.has_setting("physics/3d/water_linear_damp"):
+		ProjectSettings.set_setting("physics/3d/water_linear_damp", 1.0)
+		ProjectSettings.add_property_info({
+			"name": "physics/3d/water_linear_damp",
+			"type": TYPE_REAL,
+			"hint": PROPERTY_HINT_NONE,
+			"hint_string": "linear drag force underwater"
+		})
+	if not ProjectSettings.has_setting("physics/3d/water_angular_damp"):
+		ProjectSettings.set_setting("physics/3d/water_angular_damp", 1.0)
+		ProjectSettings.add_property_info({
+			"name": "physics/3d/water_angular_damp",
+			"type": TYPE_REAL,
+			"hint": PROPERTY_HINT_NONE,
+			"hint_string": "angular drag force underwater"
+		})
 
 func create_spatial_gizmo(for_spatial):
-	if for_spatial is water_physic:
-		var buoyancy_points = buoyancy_gizmo.new(for_spatial)
+	if for_spatial is water_physics:
+		var buoyancy_points = buoyancy_gizmo.new(self, for_spatial)
 		return buoyancy_points
 
 func _exit_tree():
 	remove_custom_type("Water")
+	remove_custom_type("BuoyantBody")
 	remove_custom_type("BuoyancyPoints")
+	
+	print("water plugin enter tree")
+
+func handles(object):
+	return true
+
+func _input(event):
+	emit_signal('input', event)
